cb26df57c9b0bdeb71e347ea9838e66ea50b7782
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-04-08 11:46:45 +0530

    fetch and update proposals

---
+++ pages/api/proposal/[id]/index.js
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth";
+import Proposal from "../../../../models/Proposal.js";
+import connectDB from "../../../../utils/db.js";
+import { authOptions } from "../../auth/[...nextauth].js";
+
+const handler = async (req, res) => {
+  connectDB();
+  const sess = await getServerSession(req, res, authOptions);
+
+  //   if (!sess) {
+  //     return res.status(200).json({ msg: "Not Authorized" });
+  //   } else {
+  const { id } = req.query;
+  const { description } = req.body;
+
+  if (req.method === "PUT") {
+    const updated = await Proposal.findByIdAndUpdate(
+      id,
+      {
+        description,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updated);
+  }
+  //   }
+};
+
+export default handler;
